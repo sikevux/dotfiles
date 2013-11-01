b103b775f1252435debc1a1b203081147042464e
Fredrik Strandin <fredrik@strandin.name>
2013-11-01 23:30:05 +0100

    Cleaned up & added some doc to bootstraper

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
-cd "$(dirname "${BASH_SOURCE}")"
-git pull origin master
+
+echo -e "Usage: bootstrap.sh [-b | -f]
+Were:
+-b means backup original files
+-f means force install\n"
 
 if [ ! -z "$1" ] && [ "$1" = "-b" ]; then
+	echo "Using backup-feature. Look for *_ORIGINAL-files in ~"
 	BACKUP="--backup --suffix=_ORIGINAL"
 else
 	BACKUP=""
 fi
+
+cd "$(dirname "${BASH_SOURCE}")"
+git pull origin master
 
 function doIt() {
 	rsync $BACKUP --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
@@ -16,7 +23,6 @@ function doIt() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
 	doIt
 else
-	echo -e "Usage: bootstrap.sh [-b]\nWere -b means backup original files"
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
