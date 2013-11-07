f7c78c945a2eacd88bea6a6861a5a88e3125e992
Fredrik Strandin <fredrik@strandin.name>
2013-11-07 16:12:37 +0100

    Added .gitmodules to exclude-list for rsync

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,8 @@ git pull origin master
 
 function doIt() {
 	rsync $BACKUP --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude ".gitmodules" \
+		-av --no-perms . ~
 	source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
